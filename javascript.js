ed435d8f63623d76bada0530805662bb0e1d8cad
James Sullivan <jesullivan222@gmail.com>
2022-10-27 19:37:41 -0400

    Use js to set gridRow and gridSquare height/width
    * gridRow and gridSquare h/w based on the h/w of gridContainer
    * CSS edited to remove redundant h/w values for gridContainer, as
      values are added via js

--- javascript.js
+++ javascript.js
@@ -1,15 +1,36 @@
-function createGrid(gridSize) {
+function createGrid(gridSize, gridDimension="960px") {
+
+    // create gridContainer div
     const gridContainer = document.createElement('div');
     gridContainer.classList.add("grid-container");
+    
 
+    // set height and width of gridContainer to 960px or given dimensions
+    gridContainer.setAttribute("style", `height: ${gridDimension}; width: ${gridDimension}`);
+
+    // append gridContainer to the page body
+    const body = document.body;
+    body.appendChild(gridContainer);
+
+    // loop to create grid rows and squares
     for (i = 0; i < gridSize; i++) {
         const gridRow = document.createElement('div');
         gridRow.classList.add('grid-row');
+
+        // set dimensions of row based on dimensions of gridContainer
+        gridRow.setAttribute("style", `height: ${gridDimension / gridSize}; width: ${gridDimension}`);
+
+        // append gridRow to gridContainer
         gridContainer.appendChild(gridRow);
 
         for (i = 0; i < gridSize; i++) {
             const gridSquare = document.createElement('div');
             gridSquare.classList.add("grid-square");
+
+            // set dimensions of square based on dimensions of gridContainer
+            gridSquare.setAttribute("style",
+             `height: ${gridDimension / gridSize}; width: ${gridDimension / gridSize}`);
+
             gridRow.appendChild(gridSquare);
         }
  
