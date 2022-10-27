beb09b7ff41a927720625963ddf992be18e501db
James Sullivan <jesullivan222@gmail.com>
2022-10-27 19:12:23 -0400

    Add loop which creates a grid squares
    * Grid squares are appended as children to grid container
    * Loop doesn't currently work as intended - must create a nested
      loop to get an n*n grid

--- javascript.js
+++ javascript.js
@@ -1,4 +1,10 @@
 function createGrid(gridSize) {
     const gridContainer = document.createElement('div');
     gridContainer.classList.add("grid-container");
+
+    for (i = 0; i < gridSize; i++) {
+        const gridSquare = document.createElement('div');
+        gridSquare.classList.add("grid-square");
+        gridContainer.appendChild(gridSquare);
+    }
 }
